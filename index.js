c7eacf40a6d4f090414eb47800f4361a62eac45b
myk <tf2myk@gmail.com>
2023-03-19 05:05:21 -0700

    Added a timer to the prediction board

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, messageLink } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, messageLink, Message } = require('discord.js');
 const { token } = require('./config.json');
+const { channel } = require('node:diagnostics_channel');
 const reactioncheck = new Map();
+const gambleboards = new Map();
+isGamble = false;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions] });
 
@@ -22,18 +25,55 @@ client.once(Events.ClientReady, () => {
 });
 
 
+
+function gambletimer(message)
+{
+	
+	//console.log(message);
+
+
+	holding = message.content;
+	holding2 = holding.concat('\n \n Voting has ended ! ! !');
+	message.edit(holding2);
+	isGamble = false;
+	
+};
+
+
+
 //client.on('messageCreate', (oldmessage, message) => {
 	client.on('messageUpdate', (oldmessage, message) => {
-	//console.log(message.content);
-	//console.log(message.author);
-	if(message.author.bot && message.author.username == "valbets" && message.content.includes("PREDICTIONS"))
-	{
-		//console.log(`The bot said: ${message.content}`);
-		console.log("Bot started predicitions");
-		message.react('😄')
-			.then(() => message.react('🙁'))
-			.catch(error => console.error('One of the emojis failed to react:', error));
-	}
+		var that = this.message;
+
+		if(message.author.bot && message.author.username == "valbets" && message.content.includes("PREDICTIONS"))
+		{
+			/*
+			if(isGamble && message.content.includes("WIN"))    /// NEED TO FIGURE OUT HOW  TO NOT CHANGE ON EDITS
+			{
+				message.edit("A prediction is currently happening")
+				setTimeout(() => message.delete(), 3000);
+			}
+			*/
+
+
+			if(!isGamble)
+			{
+				//console.log(`The bot said: ${message.content}`);
+				message.react('😄')
+					.then(() => message.react('🙁'))
+					.catch(error => console.error('One of the emojis failed to react:', error));
+
+				isGamble = true;
+
+				
+				setTimeout(gambletimer, 5000, message);
+
+				
+			}
+			
+			
+		}
+	
 });
 
 
@@ -42,7 +82,7 @@ client.on("messageReactionAdd", function(messageReaction, user){
 	{
 		
 
-		if(reactioncheck.get(user.username) != messageReaction.message.id)
+		if(reactioncheck.get(user.username) != messageReaction.message.id && !messageReaction.message.content.includes("ended"))
 		{
 			reactioncheck.set(user.username, messageReaction.message.id)
 			hold = messageReaction.message.content;
@@ -53,7 +93,7 @@ client.on("messageReactionAdd", function(messageReaction, user){
 					hold3 = hold2.concat
 				(`
 				
-				${user.username} :: WIN` 
+		${user.username} :: WIN` 
 				);
 
 				hold3 = hold3.concat("```"); 
@@ -66,7 +106,7 @@ client.on("messageReactionAdd", function(messageReaction, user){
 					hold3 = hold2.concat
 				(`
 				
-				${user.username} :: LOSE` 
+		${user.username} :: LOSE` 
 				);
 
 				hold3 = hold3.concat("```"); 
